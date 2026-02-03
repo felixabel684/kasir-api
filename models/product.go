3cfe30834bc61ef423657bc267e6ddc0d8ef0063
felixabel684 <felixabel684@gmail.com>
2026-02-03 11:46:11 +0700

    implement join category to product

--- models/product.go
+++ models/product.go
@@ -5,4 +5,6 @@ type Product struct {
 	Name  string `json:"name"`
 	Price int    `json:"price"`
 	Stock int    `json:"stock"`
+	CategoryID int    `json:"category_id"`      // ID kategori
+	Category   string `json:"category_name,omitempty"` // optional, akan diisi saat join
 }
